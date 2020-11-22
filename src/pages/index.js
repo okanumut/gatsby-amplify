5252f01bc69e07026d8d1d8cbc867f6a181b6efb
Okan Umut Acer <acer.okanumut@gmail.com>
2020-11-22 08:24:34 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Merhaba</h1>
     <p>Gatsby sitesine hosgeldiniz</p>
-    <p>Hosting olarak -- amplify !!</p>
+    <p>Hosting -- Amplify !!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
